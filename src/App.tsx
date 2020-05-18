215b879dc59f43b01edfadbacf59a0406d8c2cb8
Rafael Nai <rafael.nai@riachuelo.com.br>
2020-05-18 09:37:54 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from "react";
-import BorderEditor from "./components/border-editor";
+import BorderEditor from "./containers/border-editor";
+import preset from "jss-preset-default";
+import { jss } from "react-jss";
+
+jss.setup(preset());
+
+const styles = {
+  "@global": {
+    body: {
+      margin: 0,
+      padding: 0,
+    },
+  },
+};
+
+jss.createStyleSheet(styles).attach();
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ backgroundColor: "#4b6584", height: "100vh", width: "100vw" }}
+    >
       <BorderEditor />
     </div>
   );
